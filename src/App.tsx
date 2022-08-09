6983bca544020b6db4feb0b299ed6d0aa634bf45
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-08 21:19:52 -0500

    chore: switch from scss to sass syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import logo from './logo.svg'
-import './App.scss'
+import './App.sass'
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './index.scss'
+import './index.sass'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
