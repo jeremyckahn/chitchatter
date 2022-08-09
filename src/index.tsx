2f0bc863ca2aba3ec8513aa17024267a5f1ef80b
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-08 21:51:55 -0500

    fix: register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './index.sass'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -16,3 +17,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+serviceWorkerRegistration.register()
