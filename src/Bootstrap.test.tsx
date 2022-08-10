90379b2c03184a8a36c510069416f0439e5d83e4
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-09 21:06:16 -0500

    chore: set up basic colors

--- src/Bootstrap.test.tsx
+++ src/Bootstrap.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Bootstrap from './Bootstrap'
 
-test('renders learn react link', () => {
+test('renders', () => {
   render(<Bootstrap />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
 })
