3b1a55067d054fa5a8a6087e5c694d4def432b48
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-09 21:57:29 -0500

    feat: add basic home content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
 
 export function Home() {
-  return <div className="Home"></div>
+  return (
+    <div className="Home">
+      <header className="max-w-3xl text-center mx-auto">
+        <Typography variant="h1">chitchatter</Typography>
+        <Typography variant="body1">
+          This is a communication tool that is free, open source, and designed
+          for maximum security. All communication between you and your online
+          peers is encrypted and ephemeral.
+        </Typography>
+      </header>
+    </div>
+  )
 }
