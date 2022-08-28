d90469e06fb64b561c541581aa27f6e13a62d3e6
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-27 21:25:38 -0500

    feat: render messages as markdown

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,10 +1,39 @@
-import Typography from '@mui/material/Typography'
+import { HTMLAttributes } from 'react'
+import Box from '@mui/material/Box'
+import Typography, { TypographyProps } from '@mui/material/Typography'
+import Link, { LinkProps } from '@mui/material/Link'
+import Markdown from 'react-markdown'
 
 import { Message as IMessage, isMessageReceived } from 'models/chat'
 
 export interface MessageProps {
   message: IMessage
   userId: string
+}
+
+const typographyFactory =
+  (overrides: TypographyProps) => (args: HTMLAttributes<HTMLElement>) => {
+    return <Typography {...args} {...overrides} />
+  }
+
+const linkFactory =
+  (overrides: LinkProps) => (args: HTMLAttributes<HTMLElement>) => {
+    return <Link {...args} {...overrides} />
+  }
+
+const componentMap = {
+  h1: typographyFactory({ variant: 'h1' }),
+  h2: typographyFactory({ variant: 'h2' }),
+  h3: typographyFactory({ variant: 'h3' }),
+  h4: typographyFactory({ variant: 'h4' }),
+  h5: typographyFactory({ variant: 'h5' }),
+  h6: typographyFactory({ variant: 'h6' }),
+  p: typographyFactory({ variant: 'body1' }),
+  a: linkFactory({
+    variant: 'body1',
+    underline: 'always',
+    color: 'primary.light',
+  }),
 }
 
 export const Message = ({ message, userId }: MessageProps) => {
@@ -20,8 +49,7 @@ export const Message = ({ message, userId }: MessageProps) => {
 
   return (
     <div className="Message block">
-      <Typography
-        variant="body1"
+      <Box
         sx={{
           backgroundColor,
           margin: 0.5,
@@ -32,8 +60,10 @@ export const Message = ({ message, userId }: MessageProps) => {
           wordBreak: 'break-all',
         }}
       >
-        {message.text}
-      </Typography>
+        <Markdown components={componentMap} linkTarget="_blank">
+          {message.text}
+        </Markdown>
+      </Box>
     </div>
   )
 }
