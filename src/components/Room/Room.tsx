70ac4f34da761e28142be483105a083187b49aae
Jeremy Kahn <jeremyckahn@gmail.com>
2022-08-30 09:38:14 -0500

    feat: use relay servers when necessary

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -39,6 +39,28 @@ export function Room({
       trackerUrls: process.env.REACT_APP_TRACKER_URL
         ? [process.env.REACT_APP_TRACKER_URL]
         : undefined,
+      rtcConfig: {
+        iceServers: [
+          {
+            urls: 'stun:openrelay.metered.ca:80',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+        ],
+      },
     },
     roomId
   )
