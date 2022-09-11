db5b8b7905c3b2ce652c494b1111a80d714063a0
Jeremy Kahn <jeremyckahn@gmail.com>
2022-09-11 13:49:51 -0500

    test: log update test message

--- src/index.tsx
+++ src/index.tsx
@@ -12,3 +12,6 @@ root.render(<Bootstrap />)
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// FIXME: Remove this
+console.log('Update test')
