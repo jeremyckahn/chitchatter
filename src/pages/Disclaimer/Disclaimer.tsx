ed76224c8ae85d2978665a1f6bff49ae425af18d
Jeremy Kahn <jeremyckahn@gmail.com>
2022-10-16 18:23:48 -0500

    chore: use mui-markdown for disclaimer copy

--- src/pages/Disclaimer/Disclaimer.tsx
+++ src/pages/Disclaimer/Disclaimer.tsx
@@ -1,38 +1,10 @@
-import { useContext, useEffect, PropsWithChildren } from 'react'
+import { useContext, useEffect } from 'react'
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import MuiMarkdown from 'mui-markdown'
 
 import { ShellContext } from 'contexts/ShellContext'
 
-const H2 = ({ children }: PropsWithChildren) => (
-  <Typography
-    variant="h2"
-    sx={theme => ({
-      fontSize: theme.typography.h3.fontSize,
-      fontWeight: theme.typography.fontWeightMedium,
-      mb: 2,
-    })}
-  >
-    {children}
-  </Typography>
-)
-
-const H3 = ({ children }: PropsWithChildren) => (
-  <Typography
-    variant="h2"
-    sx={theme => ({
-      fontSize: theme.typography.h5.fontSize,
-      fontWeight: theme.typography.fontWeightMedium,
-      mb: 1.5,
-    })}
-  >
-    {children}
-  </Typography>
-)
-
-const P = ({ children }: PropsWithChildren) => (
-  <Typography sx={{ mb: 1 }}>{children}</Typography>
-)
+import './index.sass'
 
 export const Disclaimer = () => {
   const { setTitle } = useContext(ShellContext)
@@ -43,14 +15,23 @@ export const Disclaimer = () => {
 
   return (
     <Box className="max-w-3xl mx-auto p-4">
-      <H2>Interpretation and Definitions</H2>
-      <H3>Interpretation</H3>
-      <P>
-        The words of which the initial letter is capitalized have meanings
-        defined under the following conditions. The following definitions shall
-        have the same meaning regardless of whether they appear in singular or
-        in plural.
-      </P>
+      <MuiMarkdown>
+        {`
+## Interpretation and Definitions
+### Interpretation
+
+The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+
+### Definitions
+
+For the purposes of this Disclaimer:
+
+- **Company** (referred to as either "the Company", "We", "Us" or "Our" in this Disclaimer) refers to Chitchatter.
+- **Service** refers to the Website.
+- **You** means the individual accessing the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.
+- **Website** refers to Chitchatter, accessible from https://chitchatter.im
+`}
+      </MuiMarkdown>
     </Box>
   )
 }
