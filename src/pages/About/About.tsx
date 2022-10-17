5996ab440ccb9d75a7bbbfa5cb24d27eb62980e3
Jeremy Kahn <jeremyckahn@gmail.com>
2022-10-16 21:40:43 -0500

    feat: improve About page formatting

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -18,11 +18,11 @@ export const About = () => {
     <Box className="About max-w-3xl mx-auto p-4">
       <MuiMarkdown>
         {`
-## User Guide
+### User Guide
 
 Chitchatter is a communication tool designed to make secure and private communication accessible to all. Please [see the README](https://github.com/jeremyckahn/chitchatter/blob/develop/README.md) for full project documentation.
 
-### Chat rooms
+#### Chat rooms
 
 Public rooms can be joined by **anyone** with the room URL. By default, rooms are given a random and un-guessable name. You can name your room whatever you'd like, but keep in mind that simpler room names are more guessable by others. For maximum security, consider using the default room name.
 
@@ -30,7 +30,7 @@ To connect to others, share the room URL with a secure tool such as [Burner Note
 
 Chat message transcripts are erased as soon as you close the page or navigate away from the room.
 
-### Message Authoring
+#### Message Authoring
 
 Chat messages support [GitHub-flavored Markdown](https://github.github.com/gfm/).
 
