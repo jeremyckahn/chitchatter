d27453391718f93af414c39949f544e9b82f1118
Jeremy Kahn <jeremyckahn@gmail.com>
2022-10-27 22:18:42 -0500

    refactor: remove children from PasswordPrompt

--- src/components/PasswordPrompt/PasswordPrompt.tsx
+++ src/components/PasswordPrompt/PasswordPrompt.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, PropsWithChildren, useState, SyntheticEvent } from 'react'
+import { ChangeEvent, useState, SyntheticEvent } from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Dialog from '@mui/material/Dialog'
@@ -7,13 +7,12 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 
-interface PasswordPromptProps extends PropsWithChildren {
+interface PasswordPromptProps {
   isOpen: boolean
   onPasswordEntered: (password: string) => void
 }
 
 export const PasswordPrompt = ({
-  children,
   isOpen,
   onPasswordEntered,
 }: PasswordPromptProps) => {
@@ -29,41 +28,38 @@ export const PasswordPrompt = ({
   }
 
   return (
-    <>
-      {children}
-      <Dialog open={isOpen}>
-        <form onSubmit={handleFormSubmit}>
-          <DialogTitle>Room Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText sx={{ mb: 2 }}>
-              You will only be able to connect to room peers that enter the same
-              password. Due to the decentralized nature of Chitchatter, it is
-              impossible to know if the password you enter will match the
-              password entered by other peers.
-            </DialogContentText>
-            <DialogContentText>
-              If there is a mismatch, you will be in the room but be unable to
-              connect to others. An error will not be shown.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="password"
-              label="Password"
-              type="password"
-              fullWidth
-              variant="standard"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button type="submit" disabled={password.length === 0}>
-              Submit
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
-    </>
+    <Dialog open={isOpen}>
+      <form onSubmit={handleFormSubmit}>
+        <DialogTitle>Room Password</DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{ mb: 2 }}>
+            You will only be able to connect to room peers that enter the same
+            password. Due to the decentralized nature of Chitchatter, it is
+            impossible to know if the password you enter will match the password
+            entered by other peers.
+          </DialogContentText>
+          <DialogContentText>
+            If there is a mismatch, you will be in the room but be unable to
+            connect to others. An error will not be shown.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button type="submit" disabled={password.length === 0}>
+            Submit
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
   )
 }
