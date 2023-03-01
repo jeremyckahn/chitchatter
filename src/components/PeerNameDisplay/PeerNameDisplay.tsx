fba179f7ae0cd0780b3040dc86aacbc93085951e
Jeremy Kahn <jeremyckahn@gmail.com>
2023-03-01 09:39:31 -0600

    feat: display username for peers

--- src/components/PeerNameDisplay/PeerNameDisplay.tsx
+++ src/components/PeerNameDisplay/PeerNameDisplay.tsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react'
 import Typography, { TypographyProps } from '@mui/material/Typography'
+
+import { SettingsContext } from 'contexts/SettingsContext'
+import { ShellContext } from 'contexts/ShellContext'
 
 import { getPeerName } from './getPeerName'
 
@@ -7,12 +11,39 @@ interface PeerNameDisplayProps extends TypographyProps {
 }
 
 export const PeerNameDisplay = ({
-  children,
+  children: userIdToResolve,
   ...rest
 }: PeerNameDisplayProps) => {
+  const { getUserSettings } = useContext(SettingsContext)
+  const { peerList, customUsername: selfCustomUsername } =
+    useContext(ShellContext)
+
+  const { userId: selfUserId } = getUserSettings()
+
+  const isPeerSelf = selfUserId === userIdToResolve
+  const peer = peerList.find(peer => peer.userId === userIdToResolve)
+
+  const userId = isPeerSelf ? selfUserId : peer?.userId
+  const customUsername = isPeerSelf ? selfCustomUsername : peer?.customUsername
+
+  if (userId === undefined) {
+    console.error('peer lookup failed: userId is undefined')
+
+    // FIXME: This is necessary to make tests pass. An error should be thrown
+    // instead.
+    return (
+      <Typography component="span" {...rest}>
+        {getPeerName(userIdToResolve)}
+      </Typography>
+    )
+  }
+
   return (
     <Typography component="span" {...rest}>
-      {getPeerName(children)}
+      {customUsername || getPeerName(userId)}
+      {!isPeerSelf && customUsername ? (
+        <Typography variant="caption"> ({getPeerName(userId)})</Typography>
+      ) : null}
     </Typography>
   )
 }
