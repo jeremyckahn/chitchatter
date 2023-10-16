787d008686efe3b4659c1f3a0da323a4a329cd6d
Jeremy Kahn <jeremyckahn@gmail.com>
2023-10-16 09:34:44 -0500

    feat(sdk): allow subset of attributes

--- sdk/sdk.js
+++ sdk/sdk.js
@@ -1,6 +1,16 @@
 export const rootDomain = 'https://chitchatter.im/'
 
-class Chitchatter extends HTMLElement {
+// NOTE: This is a subset of standard iframe attributes:
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attributes
+const allowedAttributes = [
+  'height',
+  'width',
+  'referrerpolicy',
+  'sandbox',
+  'style',
+]
+
+class ChatEmbed extends HTMLElement {
   connectedCallback() {
     const shadow = this.attachShadow({ mode: 'open' })
     const iframe = document.createElement('iframe')
@@ -8,12 +18,16 @@ class Chitchatter extends HTMLElement {
     iframe.setAttribute('src', rootDomain)
     iframe.style.border = 'none'
 
-    for (let attributeName of this.getAttributeNames()) {
-      iframe.setAttribute(attributeName, this.getAttribute(attributeName))
+    for (let attributeName of allowedAttributes) {
+      const attributeValue = this.getAttribute(attributeName)
+
+      if (attributeValue !== null) {
+        iframe.setAttribute(attributeName, attributeValue)
+      }
     }
 
     shadow.appendChild(iframe)
   }
 }
 
-window.customElements.define('chat-embed', Chitchatter)
+window.customElements.define('chat-embed', ChatEmbed)
