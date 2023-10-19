af07e01495c139a90e8a3cf2e0934e3b9c467a24
Jeremy Kahn <jeremyckahn@gmail.com>
2023-10-19 10:07:33 -0500

    feat(sdk): [wip] handle some attribute updates

--- sdk/sdk.ts
+++ sdk/sdk.ts
@@ -7,7 +7,8 @@ export const defaultRoot = 'https://chitchatter.im/'
 
 // NOTE: This is a subset of standard iframe attributes:
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attributes
-const allowedAttributes = [
+// FIXME: Change this to be an enum
+const iframeAttributes = [
   'height',
   'referrerpolicy',
   'sandbox',
@@ -22,10 +23,17 @@ enum ChatEmbedAttributes {
 }
 
 const pollInterval = 250
-const pollTimeout = 3000
+const pollTimeout = 10_000
 
 class ChatEmbed extends HTMLElement {
   private iframe = document.createElement('iframe')
+
+  static get observedAttributes() {
+    const chatAttributes = Object.values(ChatEmbedAttributes)
+    const iframeAttributes = ['height', 'width']
+
+    return [...chatAttributes, ...iframeAttributes]
+  }
 
   get chatConfig() {
     const chatConfig: Partial<UserSettings> = {}
@@ -39,6 +47,55 @@ class ChatEmbed extends HTMLElement {
 
   get rootUrl() {
     return this.getAttribute(ChatEmbedAttributes.ROOT_URL) ?? defaultRoot
+  }
+
+  private sendConfigToChat = () => {
+    const { iframe, rootUrl } = this
+    const { origin: rootUrlOrigin } = new URL(rootUrl)
+
+    iframe.contentWindow?.postMessage(
+      {
+        name: PostMessageEventName.CONFIG,
+        payload: this.chatConfig,
+      },
+      rootUrlOrigin
+    )
+  }
+
+  private async sendConfigToChatAndWaitForConfirmation() {
+    const { rootUrl } = this
+
+    let timer: NodeJS.Timer
+    const { origin: rootUrlOrigin } = new URL(rootUrl)
+
+    const handleMessageReceived = (event: MessageEvent) => {
+      if (rootUrlOrigin !== event.origin) return
+      if (!isPostMessageEvent(event)) return
+      if (event.data.name !== PostMessageEventName.CONFIG_RECEIVED) return
+
+      clearInterval(timer)
+      window.removeEventListener('message', handleMessageReceived)
+    }
+
+    window.addEventListener('message', handleMessageReceived)
+
+    timer = setInterval(this.sendConfigToChat, pollInterval)
+
+    setTimeout(() => {
+      clearInterval(timer)
+    }, pollTimeout)
+  }
+
+  private updateIframeAttributes() {
+    const { iframe } = this
+
+    for (let attributeName of iframeAttributes) {
+      const attributeValue = this.getAttribute(attributeName)
+
+      if (attributeValue !== null) {
+        iframe.setAttribute(attributeName, attributeValue)
+      }
+    }
   }
 
   connectedCallback() {
@@ -64,14 +121,7 @@ class ChatEmbed extends HTMLElement {
     iframe.setAttribute('src', iframeSrc.href)
     iframe.style.border = 'none'
 
-    for (let attributeName of allowedAttributes) {
-      const attributeValue = this.getAttribute(attributeName)
-
-      if (attributeValue !== null) {
-        iframe.setAttribute(attributeName, attributeValue)
-      }
-    }
-
+    this.updateIframeAttributes()
     iframe.setAttribute('allow', iframeFeatureAllowList.join(';'))
 
     shadow.appendChild(iframe)
@@ -82,39 +132,18 @@ class ChatEmbed extends HTMLElement {
       chatConfig.userId = this.getAttribute(ChatEmbedAttributes.USER_ID) ?? ''
     }
 
-    this.sendConfigToChat()
+    this.sendConfigToChatAndWaitForConfirmation()
   }
 
-  private async sendConfigToChat() {
-    const { iframe, rootUrl } = this
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    // FIXME: Handle room name change
+    const isIframeAttribute = iframeAttributes.includes(name)
 
-    let timer: NodeJS.Timer
-    const { origin: rootUrlOrigin } = new URL(rootUrl)
-
-    const handleMessageReceived = (event: MessageEvent) => {
-      if (rootUrlOrigin !== event.origin) return
-      if (!isPostMessageEvent(event)) return
-      if (event.data.name !== PostMessageEventName.CONFIG_RECEIVED) return
-
-      clearInterval(timer)
-      window.removeEventListener('message', handleMessageReceived)
+    if (isIframeAttribute) {
+      this.updateIframeAttributes()
+    } else {
+      this.sendConfigToChat()
     }
-
-    window.addEventListener('message', handleMessageReceived)
-
-    timer = setInterval(() => {
-      iframe.contentWindow?.postMessage(
-        {
-          name: PostMessageEventName.CONFIG,
-          payload: this.chatConfig,
-        },
-        rootUrlOrigin
-      )
-    }, pollInterval)
-
-    setTimeout(() => {
-      clearInterval(timer)
-    }, pollTimeout)
   }
 }
 
