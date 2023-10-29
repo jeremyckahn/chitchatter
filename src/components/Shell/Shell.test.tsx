03e38c9d1f004ca5b103d2672428e6b08b599ad5
Jeremy Kahn <jeremyckahn@gmail.com>
2023-10-29 13:02:58 -0500

    fix(deps): omit ipfs-core

--- src/components/Shell/Shell.test.tsx
+++ src/components/Shell/Shell.test.tsx
@@ -28,20 +28,34 @@ const ShellStub = (shellProps: Partial<ShellProps> = {}) => {
 
 describe('Shell', () => {
   describe('menu drawer', () => {
-    test('can be opened', () => {
+    test('can be opened', async () => {
       render(<ShellStub />)
       const menuButton = screen.getByLabelText('Open menu')
-      userEvent.click(menuButton)
+      await waitFor(() => {
+        userEvent.click(menuButton)
+      })
+
       const navigation = screen.getByRole('navigation')
-      expect(navigation).toBeVisible()
+
+      await waitFor(() => {
+        expect(navigation).toBeVisible()
+      })
     })
 
     test('can be closed', async () => {
       render(<ShellStub />)
       const menuButton = screen.getByLabelText('Open menu')
-      userEvent.click(menuButton)
+
+      await waitFor(() => {
+        userEvent.click(menuButton)
+      })
+
       const closeMenu = screen.getByLabelText('Close menu')
-      userEvent.click(closeMenu)
+
+      await waitFor(() => {
+        userEvent.click(closeMenu)
+      })
+
       const navigation = screen.getByRole('navigation')
 
       await waitFor(() => {
