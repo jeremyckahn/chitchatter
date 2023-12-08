aa34d58d221d205a3a1c96a94b777dc8840d2b5d
Jeremy Kahn <jeremyckahn@gmail.com>
2023-12-07 18:24:44 -0600

    feat(verification): expire verification requests

--- src/components/Room/usePeerVerification.ts
+++ src/components/Room/usePeerVerification.ts
@@ -1,9 +1,10 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { ShellContext } from 'contexts/ShellContext'
 import { Peer, PeerVerificationState } from 'models/chat'
 import { encryptionService as encryptionServiceInstance } from 'services/Encryption'
 import { PeerRoom } from 'services/PeerRoom'
 import { PeerActions } from 'models/network'
+import { verificationTimeout } from 'config/messaging'
 
 interface UserPeerVerificationProps {
   peerRoom: PeerRoom
@@ -18,6 +19,10 @@ export const usePeerVerification = ({
 }: UserPeerVerificationProps) => {
   const { updatePeer, peerList } = useContext(ShellContext)
 
+  const verificationTimersRef = useRef<Record<Peer['peerId'], NodeJS.Timeout>>(
+    {}
+  )
+
   const [sendVerificationTokenEncrypted, receiveVerificationTokenEncrypted] =
     peerRoom.makeAction<ArrayBuffer>(PeerActions.VERIFICATION_TOKEN_ENCRYPTED)
 
@@ -31,6 +36,18 @@ export const usePeerVerification = ({
       peer.publicKey,
       verificationToken
     )
+
+    const verificationTimer = setTimeout(() => {
+      updatePeer(peer.peerId, {
+        verificationState: PeerVerificationState.UNVERIFIED,
+      })
+
+      delete verificationTimersRef.current[peer.peerId]
+
+      console.warn(`Verification for peerId ${peer.peerId} timed out`)
+    }, verificationTimeout)
+
+    verificationTimersRef.current[peer.peerId] = verificationTimer
 
     updatePeer(peer.peerId, { encryptedVerificationToken })
 
@@ -63,18 +80,30 @@ export const usePeerVerification = ({
       throw new Error(`peerId not found: ${peerId}`)
     }
 
-    if (decryptedVerificationToken !== matchingPeer.verificationToken) {
+    const { verificationToken } = matchingPeer
+
+    if (decryptedVerificationToken !== verificationToken) {
       updatePeer(peerId, {
         verificationState: PeerVerificationState.UNVERIFIED,
       })
 
       // FIXME: Surface error to the user
       throw new Error(
-        `Verification token for peerId ${peerId} does not match. [expected: ${matchingPeer.verificationToken}] [received: ${decryptedVerificationToken}]`
+        `Verification token for peerId ${peerId} does not match. [expected: ${verificationToken}] [received: ${decryptedVerificationToken}]`
       )
     }
 
-    updatePeer(peerId, { verificationState: PeerVerificationState.VERIFIED })
+    const verificationTimer = verificationTimersRef.current[peerId]
+
+    if (verificationTimer) {
+      clearTimeout(verificationTimer)
+    }
+
+    delete verificationTimersRef.current[peerId]
+
+    updatePeer(peerId, {
+      verificationState: PeerVerificationState.VERIFIED,
+    })
   })
 
   return { verifyPeer }
--- src/config/messaging.ts
+++ src/config/messaging.ts
@@ -1,2 +1,3 @@
 export const messageCharacterSizeLimit = 10_000
 export const messageTranscriptSizeLimit = 150
+export const verificationTimeout = 10_000
