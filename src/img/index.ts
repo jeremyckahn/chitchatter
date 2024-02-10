72501bb62dd6243bd87c153f286d7ec552635412
Jeremy Kahn <me@jeremyckahn.com>
2024-02-10 17:13:18 -0600

    fix(types): use correct type for SVG

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,3 +1,9 @@
-import Logo from './logo.svg'
+import LogoSVG from './logo.svg'
+
+type SVGType = React.FunctionComponent<
+  React.SVGProps<SVGSVGElement> & { title?: string }
+>
+
+const Logo = LogoSVG as unknown as SVGType
 
 export { Logo }
