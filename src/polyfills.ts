bdf18e54d62918c88ee2306049f1aaf772d23970
Jeremy Kahn <me@jeremyckahn.com>
2024-02-11 22:02:54 -0600

    fix(vite): get app to fully boot

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,1 @@
 import 'webrtc-adapter'
-import { Buffer } from 'buffer'
-
-// Polyfill
-window.Buffer = Buffer
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import path from 'path'
 
+import { fileURLToPath } from 'url'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import macrosPlugin from 'vite-plugin-babel-macros'
 
 const srcPaths = [
   'components',
@@ -23,9 +27,27 @@ const srcPathAliases = srcPaths.reduce((acc, dir) => {
 
 const config = () => {
   return defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [
+      svgr(),
+      react(),
+      macrosPlugin(),
+      nodePolyfills({
+        globals: {
+          Buffer: true, // can also be 'build', 'dev', or false
+          global: true,
+          process: true,
+        },
+        protocolImports: true,
+      }),
+    ],
     resolve: {
       alias: {
+        webtorrent: fileURLToPath(
+          new URL(
+            './node_modules/webtorrent/webtorrent.min.js',
+            import.meta.url
+          )
+        ),
         ...srcPathAliases,
       },
     },
