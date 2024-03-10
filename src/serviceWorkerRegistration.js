ba632fe052d3f7ac86c052c1aee520c968688fc9
Jeremy Kahn <me@jeremyckahn.com>
2024-03-10 18:20:46 -0500

    fix(service-worker): use VITE_HOMEPAGE for service worker resolution

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -34,7 +34,7 @@ export function register(config) {
       return
     }
 
-    const swUrl = `${import.meta.env.PUBLIC_URL}/service-worker.js`
+    const swUrl = `${import.meta.env.VITE_HOMEPAGE}/service-worker.js`
 
     if (isLocalhost) {
       // This is running on localhost. Let's check if a service worker still exists or not.
