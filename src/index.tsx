5f3dcdb9390a8dc376ef071718f70e3a665389c1
Jeremy Kahn <me@jeremyckahn.com>
2024-03-11 21:44:01 -0500

    fix(vite): make syntax highlighter work consistently
    See: https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/513

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import 'typeface-roboto'
 import './index.sass'
 
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 
 import Init from './Init'
 import reportWebVitals from './reportWebVitals'
@@ -13,6 +14,15 @@ import reportWebVitals from './reportWebVitals'
 // before it has loaded.
 // See: https://stackoverflow.com/a/76017295/470685
 ;<ThemeProvider theme={createTheme()} />
+
+// NOTE: This is a workaround for SyntaxHighlighter not working reliably in the
+// EmbedCodeDialog component. It seems to have the effect of warming some
+// sort of internal cache that avoids a race condition within
+// SyntaxHighlighter.
+// See: https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/513
+ReactDOM.createRoot(document.createElement('div')).render(
+  <SyntaxHighlighter language="" children={''} />
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<Init />)
