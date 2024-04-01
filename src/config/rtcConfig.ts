89abe718db5b88f02e6e51d68b325df835daf823
Sinisha <stojchevski0@gmail.com>
2024-04-02 00:24:25 +0200

    chore(networking): update TURN server credentials (#246)
    updated server IP address

--- src/config/rtcConfig.ts
+++ src/config/rtcConfig.ts
@@ -11,20 +11,20 @@ export const rtcConfig: RTCConfiguration = {
   // CONNECTED TO THE INTERNET.
   iceServers: [
     {
-      urls: 'stun:188.148.133.173:3478',
+      urls: 'stun:82.209.159.69:3478',
     },
     {
-      urls: 'turn:188.148.133.173:3478',
+      urls: 'turn:82.209.159.69:3478',
       username: 'c386d75b5633456cb3bc13812858098d',
       credential: '58fd06d85fe14c0f9f46220748b0f565',
     },
     {
-      urls: 'turn:188.148.133.173:3478',
+      urls: 'turn:82.209.159.69:3478',
       username: '0e2f563eacfd4c4a82ea239b04d1d494',
       credential: '8179b4b533f240ad9fe590663bef1bc9',
     },
     {
-      urls: 'turn:188.148.133.173:3478',
+      urls: 'turn:82.209.159.69:3478',
       username: 'feab95c3fcd147a2a96a3d3590bf9cda',
       credential: '654cafd885424b7fb974e65f631f25f9',
     },
