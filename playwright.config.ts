26437127568f8d13487a923cedf3acb100f6fbe2
Jeremy Kahn <me@jeremyckahn.com>
2025-08-17 17:28:17 -0500

    fixup! feat(app): prevent users from rapidly rejoining rooms

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
   /* Global timeout for each test */
   timeout: 30 * 1000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
