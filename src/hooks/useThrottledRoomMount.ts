b769f1384c1dcbfe75ce9f970fd1b98e5f30be5d
Jeremy Kahn <me@jeremyckahn.com>
2025-08-17 20:59:09 -0500

    fixup! feat(app): prevent users from rapidly rejoining rooms

--- src/hooks/useThrottledRoomMount.ts
+++ src/hooks/useThrottledRoomMount.ts
@@ -7,30 +7,37 @@ const backoffResetPeriod = 5000
 const baseBackoff = 2000
 const backoffMultiplier = 2
 
+// This hook prevents users from rapidly rejoining rooms, which can cause
+// WebRTC and peer connections to get into a broken state when leaving and
+// rejoining too quickly. It implements exponential backoff to throttle
+// successive room mounts.
+
 export function useThrottledRoomMount(roomId: string) {
   const [canMount, setCanMount] = useState(false)
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
     const now = Date.now()
-    const lastMountTime = parseInt(
-      sessionStorage.getItem(LAST_MOUNT_TIME_KEY) || '0',
-      10
-    )
+
+    const lastMountTime =
+      Number(sessionStorage.getItem(LAST_MOUNT_TIME_KEY) || '0') || 0
+
     const timeSinceLastMount = now - lastMountTime
     sessionStorage.setItem(LAST_MOUNT_TIME_KEY, now.toString())
 
-    let backoff = parseInt(sessionStorage.getItem(BACKOFF_KEY) || '0', 10)
+    let backoff = Number(sessionStorage.getItem(BACKOFF_KEY) || '0') || 0
 
     if (timeSinceLastMount < backoffResetPeriod) {
       backoff = backoff === 0 ? baseBackoff : backoff * backoffMultiplier
     } else {
       backoff = 0
     }
+
     sessionStorage.setItem(BACKOFF_KEY, backoff.toString())
 
     const mount = () => {
       setCanMount(true)
+
       timeoutRef.current = setTimeout(() => {
         sessionStorage.setItem(BACKOFF_KEY, '0')
       }, backoffResetPeriod)
